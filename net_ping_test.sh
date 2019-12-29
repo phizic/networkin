4dde588d5e7c995d8bffd8a3088b87e02e508fbd
phizic <j.chelmicki@gmail.com>
2019-12-29 12:53:56 +0100

    added ping_bang fuction

--- net_ping_test.sh
+++ net_ping_test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+ping_bang () {
+    echo "This may take a while..."
+    ping -c $1 $2 | tail -n 3
+}
+
 # ping to local resource
 echo "Pinging home router with 10 ECHO_Request"
-ping -c 10 192.168.89.10 | tail -n 3
+ping_bang 10 192.168.89.10
 # ping to a location behind nat
 echo "Pinging Google DNS with 10 ECHO_Request"
-ping -c 10 8.8.8.8 | tail -n 3
+ping_bang 10 8.8.8.8
+echo "average time for connections:"
