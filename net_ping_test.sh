289213ab0f06f6acdcfb254d2f062efaadd568a2
phizic <j.chelmicki@gmail.com>
2020-01-06 12:53:54 +0100

    Added sppedtestnet-cli execution

--- net_ping_test.sh
+++ net_ping_test.sh
@@ -6,11 +6,15 @@ ping_bang () {
 }
 
 # ping to local resource
-echo "Pinging home router with 10 ECHO_Request"
-ping_bang 10 192.168.89.10
+echo "Pinging home router with 20 ECHO_Request"
+ping_bang 20 192.168.89.10
 # ping to a location behind nat
-echo "Pinging Google DNS with 10 ECHO_Request"
-ping_bang 10 8.8.8.8
+echo "Pinging Google DNS with 20 ECHO_Request"
+ping_bang 20 8.8.8.8
+
+# displaying connection parameters suing speedtestnet-cli (should consider case in which this is not present)
+echo "Running speedtestnet..."
+speedtestnet-cli --simple
 
 # issue no.1 (currently under developement)
 echo "average time for connections:"
